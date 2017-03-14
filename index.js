ca8361f3fe0f36177ba41721d166214dd1926856
Gokhan Ozdemir <gn.ozdemir@gmail.com>
2017-03-14 15:41:48 +0300

    Source Folder as parameter + port to a mode unique one

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const fs = require('fs')
-const files = fs.readdirSync('img')
+// const sourceFolder = fs.readdirSync('ehaber')
+// const sourceFolder = fs.readdirSync('../my-other-project/src/assets/img/')
+const sourceFolder = 'img'
+const files = fs.readdirSync(sourceFolder)
+
 const http = require('http')
-const port = process.env.NOW ? 80 : 8888
+const port = process.env.NOW ? 80 : 2223
 
 for (let i = files.length - 1; i >= 0; i--) {
   if (files[i][0] === '.') files.splice(i, 1)
 }
 
 http.createServer((req, res) => {
-  const path = `img/${files[Math.floor(Math.random() * files.length)]}`
+  // const pathRel = `/assets/img/${files[Math.floor(Math.random() * files.length)]}`
+  const path = `${sourceFolder}/${files[Math.floor(Math.random() * files.length)]}`
   fs.readFile(path, (err, data) => {
     if (err) {
       res.writeHead(404, { 'Content-Type': 'text/plain' })
@@ -16,6 +21,16 @@ http.createServer((req, res) => {
     } else {
       res.writeHead(200, { 'Content-Type': 'image/jpeg' })
       res.end(data, 'binary')
+
+      // Return relative path only
+      // res.write(path);
+      // res.end();
+
+      // res.writeHead(301,
+      //   {Location: 'http://localhost:8001'+pathRel}
+      // );
+      // res.end();
+
     }
   })
 }).listen(port)
